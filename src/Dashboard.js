4440584c6128c28404dab9e7264bcb548434f1f2
chih-chien Hsiao(Daniel) <daniel13520cs@gmail.com>
2023-11-02 04:40:33 +0800

    added navbar layout

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -24,74 +24,86 @@ const data = [
     postcode: '54321',
     email: 'jane@example.com',
   },
+  {
+    id: 3,
+    firstName: 'Daneil',
+    lastName: 'Fake',
+    gender: 'Male',
+    age: 25,
+    postcode: '321',
+    email: 'Daneil@example.com',
+  },
   // Add more data as needed
 ];
 
 function Dashboard() {
   return (
-    <div>
-      <Timeline>
-        <Timeline.Item>16:27:41 Your order starts processing</Timeline.Item>
-        <Timeline.Item>16:28:43 Your order to be ready for delivery</Timeline.Item>
-        <Timeline.Item>16:28:45 Your parcel has been out of the library</Timeline.Item>
-        <Timeline.Item>02:34:41 Send to Shanghai Hongkou Company</Timeline.Item>
-        <Timeline.Item>15:05:29 Sending you a piece</Timeline.Item>
-      </Timeline>
+    <div className="dashboard">
+        <div className="timeline dashboard"> {/* Apply the 'timeline' class */}
+        <Timeline>
+            <Timeline.Item>16:27:41 Your order starts processing</Timeline.Item>
+            <Timeline.Item>16:28:43 Your order to be ready for delivery</Timeline.Item>
+            <Timeline.Item>16:28:45 Your parcel has been out of the library</Timeline.Item>
+            <Timeline.Item>02:34:41 Send to Shanghai Hongkou Company</Timeline.Item>
+            <Timeline.Item>15:05:29 Sending you a piece</Timeline.Item>
+        </Timeline>
+        </div>
+        <div className="table-container dashboard">
+            <Table
+                height={400}
+                data={data}
+                onRowClick={rowData => {
+                console.log(rowData);
+                }}
+            >
+                <Column width={60} align="center" fixed>
+                <HeaderCell>Id</HeaderCell>
+                <Cell dataKey="id" />
+                </Column>
 
-      <Table
-        height={400}
-        data={data}
-        onRowClick={rowData => {
-          console.log(rowData);
-        }}
-      >
-        <Column width={60} align="center" fixed>
-          <HeaderCell>Id</HeaderCell>
-          <Cell dataKey="id" />
-        </Column>
+                <Column width={150}>
+                <HeaderCell>First Name</HeaderCell>
+                <Cell dataKey="firstName" />
+                </Column>
 
-        <Column width={150}>
-          <HeaderCell>First Name</HeaderCell>
-          <Cell dataKey="firstName" />
-        </Column>
+                <Column width={150}>
+                <HeaderCell>Last Name</HeaderCell>
+                <Cell dataKey="lastName" />
+                </Column>
 
-        <Column width={150}>
-          <HeaderCell>Last Name</HeaderCell>
-          <Cell dataKey="lastName" />
-        </Column>
+                <Column width={100}>
+                <HeaderCell>Gender</HeaderCell>
+                <Cell dataKey="gender" />
+                </Column>
 
-        <Column width={100}>
-          <HeaderCell>Gender</HeaderCell>
-          <Cell dataKey="gender" />
-        </Column>
+                <Column width={100}>
+                <HeaderCell>Age</HeaderCell>
+                <Cell dataKey="age" />
+                </Column>
 
-        <Column width={100}>
-          <HeaderCell>Age</HeaderCell>
-          <Cell dataKey="age" />
-        </Column>
+                <Column width={150}>
+                <HeaderCell>Postcode</HeaderCell>
+                <Cell dataKey="postcode" />
+                </Column>
 
-        <Column width={150}>
-          <HeaderCell>Postcode</HeaderCell>
-          <Cell dataKey="postcode" />
-        </Column>
+                <Column width={300}>
+                <HeaderCell>Email</HeaderCell>
+                <Cell dataKey="email" />
+                </Column>
 
-        <Column width={300}>
-          <HeaderCell>Email</HeaderCell>
-          <Cell dataKey="email" />
-        </Column>
+                <Column width={80} fixed="right">
+                <HeaderCell>...</HeaderCell>
 
-        <Column width={80} fixed="right">
-          <HeaderCell>...</HeaderCell>
-
-          <Cell style={{ padding: '6px' }}>
-            {rowData => (
-              <Button appearance="link" onClick={() => alert(`id:${rowData.id}`)}>
-                Edit
-              </Button>
-            )}
-          </Cell>
-        </Column>
-      </Table>
+                <Cell style={{ padding: '6px' }}>
+                    {rowData => (
+                    <Button appearance="link" onClick={() => alert(`id:${rowData.id}`)}>
+                        Edit
+                    </Button>
+                    )}
+                </Cell>
+                </Column>
+            </Table>
+        </div>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import './container.less';
+
+
+import { Container, Header, Content, Footer, Navbar, Nav } from 'rsuite';
+import HomeIcon from '@rsuite/icons/legacy/Home';
+import CogIcon from '@rsuite/icons/legacy/Cog';
+
+const Home = () => (
+  <div className="show-fake-browser navbar-page">
+    <Container>
+      <Header>
+        <Navbar appearance="inverse">
+          <Navbar.Brand>
+            <a style={{ color: '#fff' }}>Brand</a>
+          </Navbar.Brand>
+          <Nav>
+            <Nav.Item icon={<HomeIcon />}>Home</Nav.Item>
+            <Nav.Item>News</Nav.Item>
+            <Nav.Item>Products</Nav.Item>
+            <Nav.Menu title="About">
+              <Nav.Item>Company</Nav.Item>
+              <Nav.Item>Team</Nav.Item>
+              <Nav.Item>Contact</Nav.Item>
+            </Nav.Menu>
+          </Nav>
+          <Nav pullRight>
+            <Nav.Item icon={<CogIcon />}>Settings</Nav.Item>
+          </Nav>
+        </Navbar>
+      </Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Container>
+  </div>
+);
+
+export default Home;
