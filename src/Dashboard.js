f2ea60747f38551509e664eee3c0cde9934efdeb
chih-chien Hsiao(Daniel) <daniel13520cs@gmail.com>
2023-11-01 13:27:25 +0800

    added timeline and table from rsuite

---
+++ src/Dashboard.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Table, Button, Timeline} from 'rsuite';
+import './styles.css';
+import './timeline.less'
+const { Column, HeaderCell, Cell } = Table;
+
+// Define a placeholder data array
+const data = [
+  {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+    gender: 'Male',
+    age: 30,
+    postcode: '12345',
+    email: 'john@example.com',
+  },
+  {
+    id: 2,
+    firstName: 'Jane',
+    lastName: 'Smith',
+    gender: 'Female',
+    age: 25,
+    postcode: '54321',
+    email: 'jane@example.com',
+  },
+  // Add more data as needed
+];
+
+function Dashboard() {
+  return (
+    <div>
+      <Timeline>
+        <Timeline.Item>16:27:41 Your order starts processing</Timeline.Item>
+        <Timeline.Item>16:28:43 Your order to be ready for delivery</Timeline.Item>
+        <Timeline.Item>16:28:45 Your parcel has been out of the library</Timeline.Item>
+        <Timeline.Item>02:34:41 Send to Shanghai Hongkou Company</Timeline.Item>
+        <Timeline.Item>15:05:29 Sending you a piece</Timeline.Item>
+      </Timeline>
+
+      <Table
+        height={400}
+        data={data}
+        onRowClick={rowData => {
+          console.log(rowData);
+        }}
+      >
+        <Column width={60} align="center" fixed>
+          <HeaderCell>Id</HeaderCell>
+          <Cell dataKey="id" />
+        </Column>
+
+        <Column width={150}>
+          <HeaderCell>First Name</HeaderCell>
+          <Cell dataKey="firstName" />
+        </Column>
+
+        <Column width={150}>
+          <HeaderCell>Last Name</HeaderCell>
+          <Cell dataKey="lastName" />
+        </Column>
+
+        <Column width={100}>
+          <HeaderCell>Gender</HeaderCell>
+          <Cell dataKey="gender" />
+        </Column>
+
+        <Column width={100}>
+          <HeaderCell>Age</HeaderCell>
+          <Cell dataKey="age" />
+        </Column>
+
+        <Column width={150}>
+          <HeaderCell>Postcode</HeaderCell>
+          <Cell dataKey="postcode" />
+        </Column>
+
+        <Column width={300}>
+          <HeaderCell>Email</HeaderCell>
+          <Cell dataKey="email" />
+        </Column>
+
+        <Column width={80} fixed="right">
+          <HeaderCell>...</HeaderCell>
+
+          <Cell style={{ padding: '6px' }}>
+            {rowData => (
+              <Button appearance="link" onClick={() => alert(`id:${rowData.id}`)}>
+                Edit
+              </Button>
+            )}
+          </Cell>
+        </Column>
+      </Table>
+    </div>
+  );
+}
+
+export default Dashboard;
