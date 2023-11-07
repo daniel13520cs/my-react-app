a8792607185c5972f2e937dd323b88b3cf4b3836
chih-chien Hsiao(Daniel) <daniel13520cs@gmail.com>
2023-11-07 15:44:10 +0800

    added login button functionality

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,7 @@ import {
     Container,
     Header,
     Content,
-    Footer,
+    Schema,
     Form,
     ButtonToolbar,
     Button,
@@ -11,8 +11,29 @@ import {
     FlexboxGrid
   } from 'rsuite';
   import NavBar from './NavBar';
+import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Import the necessary components
 
   function Login () {
+    const { StringType, NumberType } = Schema.Types;
+    const model = Schema.Model({
+        name: StringType().isRequired('This field is required.'),
+        email: StringType()
+          .isEmail('Please enter a valid email address.')
+          .isRequired('This field is required.')
+      });
+    const formRef = React.useRef();
+    const [formError, setFormError] = React.useState({});
+    const [formValue, setFormValue] = React.useState({
+      name: '',
+      password: '',
+    });
+    const navigate = useNavigate(); 
+    const onSigninClicked = () => {
+        navigate('/Dashboard');
+        console.log(formValue);
+    }
+
     return (
             <div className="show-fake-browser login-page">
             <Container>
@@ -23,7 +44,13 @@ import {
                 <FlexboxGrid justify="center">
                     <FlexboxGrid.Item colspan={12}>
                     <Panel header={<h3>Login</h3>} bordered>
-                        <Form fluid>
+                        <Form 
+                            ref={formRef}
+                            onChange={setFormValue}
+                            onCheck={setFormError}
+                            formValue={formValue}
+                            model={model}
+                            fluid>
                         <Form.Group>
                             <Form.ControlLabel>Username or email address</Form.ControlLabel>
                             <Form.Control name="name" />
@@ -34,7 +61,7 @@ import {
                         </Form.Group>
                         <Form.Group>
                             <ButtonToolbar>
-                            <Button appearance="primary">Sign in</Button>
+                            <Button appearance="primary" onClick={onSigninClicked}>Sign in</Button>
                             <Button appearance="primary">Register</Button>
                             <Button appearance="link">Forgot password?</Button>
                             </ButtonToolbar>
