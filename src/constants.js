3963533c137dd1a90f9d81099e5f3b682726c862
chih-chien Hsiao(Daniel) <daniel13520cs@gmail.com>
2023-11-24 02:15:33 +0800

    Updates

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-//export const apiURL = 'http://localhost:5271/'
-export const apiURL = 'https://webapi20231123120505.azurewebsites.net/';
-
-export const apiIMGURL = 'https://webapi20231123120505.scm.azurewebsites.net/';
